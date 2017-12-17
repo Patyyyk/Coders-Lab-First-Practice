49aa45c7c671b92a8adf749d2a2eef3329a664ec
Piotr <patyyyk@gmail.com>
2017-12-17 23:02:21 +0100

    Funkcjonalnosci kalkulator

--- JS/actions.js
+++ JS/actions.js
@@ -23,7 +23,72 @@ var prevButton = document.querySelector('.slider-prevbutton'),
     }
     sliderChildren[imageIndex].style.display = 'block';
   });
+  // ----------------------calculator------------------------------------------
+  var listArrow = document.querySelectorAll('.list_arrow'),
+      elementsList = document.querySelectorAll('.list_panel'),
+      chairList = elementsList[0].children,
+      colorList = elementsList[1].children,
+      patternList = elementsList[2].children,
+      yourChair = document.querySelector('.title'),
+      titleValue = document.querySelector('.title.value'),
+      spanColor = document.querySelector('.color'),
+      colorValue = document.querySelector('.color.value'),
+      spanPattern = document.querySelector('.pattern'),
+      patternValue = document.querySelector('.pattern.value'),
+      transport = document.getElementById('transport'),
+      sumStrong = document.querySelector('.sum').firstChild,
+      chairPrice = 0, colorPrice = 0, patternPrice = 0, transportPrice = 0,sum= 0;
 
+// sum function
+function sumFunc(chairPrice, colorPrice, patternPrice, transportPrice) {
+  sum = (chairPrice+colorPrice+patternPrice+transportPrice);
+  sumStrong.innerText = sum + 'zł'
+};
 
+// all arrows buttons
+  for (var i = 0; i < listArrow.length; i++) {
+    listArrow[i].addEventListener('click', function(event){
+      this.nextElementSibling.classList.toggle("visible");
+    })
+  }
+//list of chairs
+  for (var i = 0; i < chairList.length; i++) {
+    chairList[i].addEventListener('click', function(event){
+      yourChair.innerText = this.innerText;
+      titleValue.innerText = this.dataset.price;
+      this.parentElement.classList.toggle("visible");
+      chairPrice = parseInt(this.dataset.price);
+      sumFunc(chairPrice, colorPrice, patternPrice, transportPrice);
+    })
+  }
+//list of colors
+for (var i = 0; i < colorList.length; i++) {
+  colorList[i].addEventListener('click', function(event){
+    spanColor.innerText = this.innerText;
+    colorValue.innerText = this.dataset.price;
+    this.parentElement.classList.toggle("visible");
+    colorPrice = parseInt(this.dataset.price);
+    sumFunc(chairPrice, colorPrice, patternPrice, transportPrice);
+  })
+}
+  //list of patterns
+  for (var i = 0; i < patternList.length; i++) {
+    patternList[i].addEventListener('click', function(event){
+      spanPattern.innerText = this.innerText;
+      patternValue.innerText = this.dataset.price;
+      this.parentElement.classList.toggle("visible");
+      patternPrice = parseInt(this.dataset.price);
+      sumFunc(chairPrice, colorPrice, patternPrice, transportPrice);
+    })
+}
+//transport included event
+transport.addEventListener('click',function(event){
+  if (transport.checked) {
+      transportPrice = parseInt(this.dataset.price);
+}else{
+  transportPrice = 0;
+}
+sumFunc(chairPrice, colorPrice, patternPrice, transportPrice);
+})
 
 });
